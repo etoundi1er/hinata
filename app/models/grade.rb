e990d8176f15f97fa5c44691dff7a75b33891033
etoundi1er <frank.etoundi@gmail.com>
2015-04-28 18:36:27 +0000

    validates student uniqueness in grades

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,8 +1,7 @@
 class Grade < ActiveRecord::Base
   belongs_to :student
   belongs_to :exam
-  validates :student, presence: true
-#   validates :student, uniqueness: true
+  validates :student, uniqueness: true
   validates_numericality_of :score
   
 end
